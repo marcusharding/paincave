1198701b2f3d7f4c0520f3d8a7ebf27451870714
marcusharding <marcus.harding01@googlemail.com>
2019-11-23 20:54:52 +0000

    Fix tailwind issue

--- src/components/03-components/app/App.jsx
+++ src/components/03-components/app/App.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import Header from '../header/header';
 import HomepageHero from '../homepageHero/homepageHero';
+import './app.scss';
 
 class App extends React.Component {
   render() {
     return ( 
       <div className="app">
+        {/** HEADER */}
         <Header />
-        <HomepageHero />
+
+        {/** HOMPAGE HERO */}
+        <section>
+          <HomepageHero />
+        </section>
+
+        {/** ABOUT */}
+        <section className="h-screen aboutBg">
+          
+        </section>
+
+        {/** SHOP */}
+        <section className="h-screen">
+          
+        </section>
+
+        {/** GYM */}
+        <section className="h-screen">
+          
+        </section>
+
+        {/** ATHLETES */}
+        <section className="h-screen">
+          
+        </section>
       </div>
     );    
   }
