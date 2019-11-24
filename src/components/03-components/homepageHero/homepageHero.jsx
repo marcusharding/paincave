d2a5d4101ebba2ddd3b31a39d86a653f5e323ca5
marcusharding <marcus.harding01@googlemail.com>
2019-11-24 22:03:03 +0000

    Develop hamburger as per designs |header text z-index

--- src/components/03-components/homepageHero/homepageHero.jsx
+++ src/components/03-components/homepageHero/homepageHero.jsx
@@ -9,11 +9,13 @@ class HomepageHero extends React.Component {
                     <div className="h-full w-full wrapper justify-center flex flex-col items-center">
                         <div className="absolute headerAthlete flex justify-center">
                             <div className="w-full h-full relative aspect-ratio">
-                                <img className="absolute inset-0 object-cover w-full h-full" alt="" src={require('../../../assets/img/header_athlete.png')}></img>
+                                <img className="absolute inset-0 object-cover w-full h-full lg:z-10" alt="" src={require('../../../assets/img/header_athlete.png')}></img>
                             </div>
                         </div>
-                        <p className="uppercase text-offWhite text-3xl lg:text-6xl"><span className="lg:mx-24">lorem</span> <span className="lg:mx-24">ipsum</span></p>
-                        <p className="uppercase text-primary text-3xl lg:text-6xl"><span className="lg:mx-24">sit</span> <span className="lg:mx-24">dolor</span></p>
+                        <div className="z-10 lg:z-0">
+                            <p className="uppercase text-offWhite text-3xl lg:text-6xl"><span className="lg:mx-24">lorem</span> <span className="lg:mx-24">ipsum</span></p>
+                            <p className="uppercase text-primary text-3xl lg:text-6xl"><span className="lg:mx-24">sit</span> <span className="lg:mx-24">dolor</span></p>
+                        </div>
                     </div>
                 </div>
 
