afdd9a7febaddee657d495c0c6239622b65166dc
marcusharding <marcus.harding01@googlemail.com>
2020-01-04 17:36:45 +0000

    Add button accessiblity

--- src/components/01-objects/logo.jsx
+++ src/components/01-objects/logo.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class logo extends React.Component {
     render() {
         return (
-            <a className={`navbar-brand uppercase font-bold tracking-wider z-10 ${this.props.state ? 'fade-in-extended': ''}`} data-scroll href="#hero">
-	            <p className="text-offWhite">pain<span className="text-primary">cave</span></p>
+            <a className={`navbar-brand uppercase tracking-wider z-10 ${this.props.state ? 'fade-in-extended': ''}`} data-scroll href="#hero">
+	            <p className="text-offWhite font-bold">pain<span className="text-primary font-bold">cave</span></p>
             </a>
         );
     }
